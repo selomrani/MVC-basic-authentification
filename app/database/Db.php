8d3066428611e1abd7744c3f25a010a5e9d4ab4d
Soufyan <elomranisoufyan@gmail.com>
2026-01-15 08:52:14 +0100

    added db connect class

---
+++ app/database/Db.php
@@ -0,0 +1,30 @@
+<?php
+namespace App\Database;
+
+use PDO;
+use PDOException;
+class Db {
+    private static $conn = null; 
+    private $pdo;
+
+    private $host = 'db';
+    private $user = 'root';
+    private $pass = '77';
+    private $dbname = 'SwiftDropDB';
+
+    private function __construct() {
+        $dsn = 'mysql:host=' . $this->host . ';dbname=' . $this->dbname;
+        try {
+            $this->pdo = new PDO($dsn, $this->user, $this->pass);
+            $this->pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+        } catch (PDOException $e) {
+            die("Connection Error: " . $e->getMessage());
+        }
+    }
+    public static function connect() {
+        if (self::$conn === null) {
+            self::$conn = new self();
+        }
+        return self::$conn->pdo;
+    }
+}
